8fa153f44983cbf2163fc880abb0570a3ae87f00
Ignatov Dan <ignatovdan@gmail.com>
2018-05-10 17:45:01 +0300

    Add EventEmitter alternative as mixin

--- src/view/event-emitter.js
+++ src/view/event-emitter.js
@@ -1,20 +1,19 @@
 export default class EventEmitter { // implemented like 'Event' in WEB API
   // Alternatives:
-  // - implement like EventEmitter, NodeJS - this code is for browsers, new npm module?
-  // - 'function AddEventEmitter(obj)':  add new methods to each passed object - memory problems?
-  // - 'function AddEventEmitter(TargetFunctionForNew)': add new methods to the 'TargetFunctionForNew' - 'class extends' is the same
-  // - 'function AddEventEmitter(targetClass)': add new methods to the 'targetClass' - 'class extends' is the same
-
-  constructor() {
-    this._events = {};
-  }
+  // 1. implement like EventEmitter, NodeJS - this code is for browsers, new npm module?
+  // 2. 'function AddEventEmitter(obj)':  add new methods to each passed object - memory problems?
+  // 3. 'function AddEventEmitter(TargetFunctionForNew)': add new methods to the 'TargetFunctionForNew' - 'class extends' is the same
+  // 4. 'function AddEventEmitter(targetClass)': add new methods to the 'targetClass' - 'class extends' is the same
+  // 5. mixin: there is no 'constructor' and methods can be directly copied to the 'target', the 'this._events' field will be created in each object on the first access. - 'class extends' is the same, multiple inheritance is not requared at this stage.
 
   // event.type is the string, name of the event that should be passed to the addEventListener
   //(implemented like 'Event.type' in WEB API)
   dispatchEvent(event) { //like 'emit' in NodeJS but there is a single object instead of a bunch of arguments
-    const eventListeners = this._events[event.type];
-    if(eventListeners) {
-      eventListeners.forEach(listener => listener(event));
+    if(this._events) {
+      const eventListeners = this._events[event.type];
+      if(eventListeners) {
+        eventListeners.forEach(listener => listener(event));
+      }
     }
   }
 
@@ -22,6 +21,8 @@ export default class EventEmitter { // implemented like 'Event' in WEB API
   // listener - A callback function that accepts a single parameter: an object based on Event describing the event which has occurred
   // (implemented like 'Event.type' in WEB API)
   addEventListener(event, listener) { //'on' in NodeJS
+    if(!this._events) this._events = {};
+
     let eventListeners = this._events[event];
     if(!eventListeners) {
       eventListeners = new Array();
